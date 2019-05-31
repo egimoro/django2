bee2a7d1861490d5eafb871c87e7610f50545d7c
egimoro <gimoremma@gmail.com>
2019-05-31 18:58:06 +0200

    Simple polls app for beginners

---
+++ webtodo/django-polls/polls/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import datetime
+
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Question(models.Model):
+    __tablename__ = 'polls_question'
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Choice(models.Model):
+    __tablename__ = 'polls_choice'
+    question = models.ForeignKey(Question,
+                                 on_delete=models.CASCADE, default='pk')
+    votes = models.IntegerField(default=0)
+    choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
